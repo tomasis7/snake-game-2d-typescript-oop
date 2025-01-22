2db50c776766e0b7967ff79d250777b2c3ec655f
tomasis7 <tomasis7@gmail.com>
2025-01-22 10:49:20 +0100

    add scrolling functionality to LevelFactory draw method

--- src/levelfactory.ts
+++ src/levelfactory.ts
@@ -1,13 +1,18 @@
 // Level Factory
 class LevelFactory {
   private gridSize: number = 32;
+  private scrollSpeed: number = 2;
+  private cameraOffset: number = 0;
 
   draw(): void {
     // Draw level creation elements
     push();
     stroke(150, 150, 150);
     strokeWeight(2);
-    for (let x = 0; x < width * 2; x += this.gridSize) {
+
+    this.cameraOffset += this.scrollSpeed;
+
+    for (let x = -this.cameraOffset; x < width * 2; x += this.gridSize) {
       line(x, 0, x, height);
     }
     for (let y = 0; y < height; y += this.gridSize) {
