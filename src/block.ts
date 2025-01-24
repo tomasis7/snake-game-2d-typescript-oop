6985e8c7e38837abf1bce1c1568addfd09fa26f0
gitwilma <wilma.haakansson@gmail.com>
2025-01-24 20:50:34 +0100

    visual block through levelfactory

--- src/block.ts
+++ src/block.ts
@@ -1,20 +1,23 @@
-// class Block extends Entity {
-//     constructor(x: number, y: number) {
-//       super(
-//         createVector(x, y),
-//         createVector(50, 50),
-//         null, // Ingen bild behövs
-//         0,
-//         0,
-//         createVector(0, 0)
-//       );
-//     }
+class Block {
+    position: p5.Vector;
+    size: p5.Vector;
+    color: string;
   
-//     // anpassning av block
-//     draw(): void {
-//       fill(255, 223, 0); // Gul färg
-//       noStroke();
-//       rect(this.position.x, this.position.y, this.size.x, this.size.y);
-//     }
-//   }
+    constructor(x: number, y: number, color: string) {
+      this.position = createVector(x, y);
+      this.size = createVector(50, 50); // Standardstorlek
+      this.color = color;
+    }
+  
+    draw(): void {
+      push();
+      fill(this.color);
+      noStroke();
+      rect(this.position.x, this.position.y, this.size.x, this.size.y);
+      pop();
+    }
+  
+    update(): void {
+    }
+  }
   
