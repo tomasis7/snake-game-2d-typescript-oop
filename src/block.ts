2f8364162f418bbd82f439b5e4a58adba2648d8c
gitwilma <wilma.haakansson@gmail.com>
2025-01-24 21:25:28 +0100

    added yOffset and xOffset for levelfactory, drawn wall with Block

--- src/block.ts
+++ src/block.ts
@@ -1,18 +1,21 @@
 class Block {
     position: p5.Vector;
     size: p5.Vector;
-    color: string;
+    fillColor: string;
+    strokeColor: string;
   
-    constructor(x: number, y: number, color: string) {
+    constructor(x: number, y: number, fillColor: string, strokeColor: string) {
       this.position = createVector(x, y);
-      this.size = createVector(50, 50); // Standardstorlek
-      this.color = color;
+      this.size = createVector(32, 32);
+      this.fillColor = fillColor;
+      this.strokeColor = strokeColor;
     }
   
     draw(): void {
       push();
-      fill(this.color);
-      noStroke();
+      fill(this.fillColor);
+      stroke(this.strokeColor);
+      strokeWeight(2);
       rect(this.position.x, this.position.y, this.size.x, this.size.y);
       pop();
     }
