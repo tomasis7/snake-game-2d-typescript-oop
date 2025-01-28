e35e905e6c8bdb67769df341ed9733f2c137063e
Peter <peter.warholm@gmail.com>
2025-01-28 19:30:36 +0100

    Updated Sketch. Removed unused assets and code

--- src/sketch.ts
+++ src/sketch.ts
@@ -3,23 +3,13 @@
 let game: Game;
 
 let images: {
-  head1: p5.Image;
-  trail1: p5.Image;
-  head2: p5.Image;
-  trail2: p5.Image;
   star: p5.Image;
   heart: p5.Image;
   ghost: p5.Image;
   Plant: p5.Image;
-  tetrisZ: p5.Image;
-  tetrisCube: p5.Image;
-  // tetrisJ: p5.Image;
-  // tetrisT: p5.Image;
-  // tetrisHero: p5.Image;
 };
 
 let music: {
-  mystery: p5.SoundFile;
   background: p5.SoundFile;
 };
 
@@ -43,8 +33,7 @@ let showGrid: boolean = false; // Toggle for grid visibility
 function preload() {
   // Load assets here
   music = {
-    mystery: loadSound("/assets/music/mystery.mp3"),
-    background: loadSound("/assets/music/mario-background.mp3"),
+    background: loadSound("/assets/music/mario-background.mp3")
   };
 
   sounds = {
@@ -54,23 +43,14 @@ function preload() {
     ghost: loadSound("/assets/sounds/ghost.mp3"),
     starPickUp: loadSound("/assets/sounds/star.mp3"),
     winner: loadSound("/assets/sounds/winner.mp3"),
-    blockCollision: loadSound("/assets/sounds/error.mp3"),
+    blockCollision: loadSound("/assets/sounds/error.mp3")
   };
 
   images = {
-    head1: loadImage("/assets/images/snake-head-blue.png"),
-    trail1: loadImage("/assets/images/snake-trail-blue.png"),
-    head2: loadImage("/assets/images/snake-head-pink.png"),
-    trail2: loadImage("/assets/images/snake-trail-pink.png"),
     star: loadImage("/assets/images/star.webp"),
     heart: loadImage("/assets/images/heart.webp"),
     ghost: loadImage("assets/images/ghost.png"),
-    Plant: loadImage("/assets/images/plant.gif"),
-    tetrisZ: loadImage("/assets/images/tetrisZ.png"),
-    tetrisCube: loadImage("/assets/images/tetrisCube.webp"),
-    // tetrisJ: loadImage("/assets/images/tetris.png"),
-    // tetrisT: loadImage("/assets/images/tetris.png"),
-    // tetrisHero: loadImage("/assets/images/tetris.png"),
+    Plant: loadImage("/assets/images/plant.gif")
   };
 
   customFont = loadFont("/assets/fonts/PressStart2P-Regular.ttf");
