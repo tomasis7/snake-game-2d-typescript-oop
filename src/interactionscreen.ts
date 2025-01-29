b0c53a3c6fe03e9dc0c3ea213dd1b701ebd0c791
Peter <peter.warholm@gmail.com>
2025-01-29 13:17:12 +0100

    Added Game name in StartMenu

--- src/interactionscreen.ts
+++ src/interactionscreen.ts
@@ -32,11 +32,14 @@ class InteractionScreen extends GameScreen {
         fill("#FFFFFF");
         textSize(28);
         text("Use the following keys on you keyboard", width / 2, height / 3);
-
+        
+        fill("#00FFFF");
         text("Player 1", width / 2 -200, height / 2 -60);
+        fill("#FF00FF");
         text("Player 2", width / 2 +200, height / 2 -60);
 
         // Arrow Controls
+        fill("#FFFFFF");
         textSize(28);
         text("↑", width / 2 -200, height / 2);
         text("← ↓ →", width / 2 -200, height / 2 +60);
