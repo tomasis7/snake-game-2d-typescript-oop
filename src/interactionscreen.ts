a5016da97ef7d1c6e818254f4f3a1938f5ee8c13
Peter <peter.warholm@gmail.com>
2025-01-29 18:39:32 +0100

    Right-side Obstacles, Moved Navigation info down

--- src/interactionscreen.ts
+++ src/interactionscreen.ts
@@ -18,7 +18,7 @@ class InteractionScreen extends GameScreen {
     }
 
     draw(): void {
-        push()
+        push();
         background("black");
 
         // Title
@@ -44,8 +44,6 @@ class InteractionScreen extends GameScreen {
         text("= +1 Life", width / 2 -300, height / 2 -160);
         image(images.star, width / 2 -330, height / 2 -120, 25, 25);
         text("= x2 Points", width / 2 -300, height / 2 -120);
-        image(images.star, width / 2 -330, height / 2 -80, 25, 25);
-        text("= x2 Points", width / 2 -300, height / 2 -80);
 
         // Obstacles / Power-downs
         textSize(24);
@@ -53,52 +51,67 @@ class InteractionScreen extends GameScreen {
         fill("#45FF8C");
         text("Obstacles", width / 2 +100, height / 2 -200);
         fill("#CCCCCC");
-        image(images.heart, width / 2 -330, height / 2 -160, 25, 25);
-        text("= +1 Life", width / 2 -300, height / 2 -160);
-        image(images.star, width / 2 -330, height / 2 -120, 25, 25);
-        text("= x2 Points", width / 2 -300, height / 2 -120);
-        image(images.star, width / 2 -330, height / 2 -80, 25, 25);
-        text("= x2 Points", width / 2 -300, height / 2 -80);
-        
+        image(images.Plant, width / 2 +70, height / 2 -160, 21, 40);
+        text("= -2 Life", width / 2 +100, height / 2 -160);
+        image(images.ghost, width / 2 +70, height / 2 -120, 35, 35);
+        text("= -1 Life, -5 Points", width / 2 +100, height / 2 -120);
+        rectMode(CENTER);
+        fill("#FDD03C");
+        stroke("#D6B034");
+        strokeWeight(2);
+        rect(width / 2 +70, height / 2 -80, 25, 25);
+        fill("#CCCCCC");
+        noStroke();
+        text("= -1 Life, Stunned", width / 2 +100, height / 2 -80);
+        fill("#8E8E8E");
+        stroke("#515151");
+        strokeWeight(2);
+        rect(width / 2 +70, height / 2 -40, 25, 25);
+        fill("#CCCCCC");
+        noStroke();
+        text("= -1 Life, Stunned", width / 2 +100, height / 2 -40);
+        pop();
 
-        // Info and players
+        // Info about navigation
+        push();
         textAlign(CENTER, CENTER);
         fill("#FFFFFF");
         textSize(28);
-        text("Use the following keys on you keyboard\nto navigate your snake in the game", width / 2, height / 2);
+        text("Use the following keys on you keyboard\nto navigate your snake in the game", width / 2, height / 2 +40);
         
+        // Players
         fill("#00FFFF");
-        text("Player 1", width / 2 -200, height / 2 +80);
+        text("Player 1", width / 2 -200, height / 2 +120);
         fill("#FF00FF");
-        text("Player 2", width / 2 +200, height / 2 +80);
+        text("Player 2", width / 2 +200, height / 2 +120);
 
         // Arrow Controls
         fill("#FFFFFF");
         textSize(28);
-        text("↑", width / 2 -200, height / 2 +140);
-        text("← ↓ →", width / 2 -200, height / 2 +200);
+        text("↑", width / 2 -200, height / 2 +180);
+        text("← ↓ →", width / 2 -200, height / 2 +240);
         noFill();
         strokeWeight(2);
         stroke(255);
         rectMode(CENTER);
-        square(width / 2 -200, height / 2 +140, 50);
-        square(width / 2 -200, height / 2 +200, 50);
-        square(width / 2 -140, height / 2 +200, 50);
-        square(width / 2 -260, height / 2 +200, 50);
+        square(width / 2 -200, height / 2 +180, 50);
+        square(width / 2 -200, height / 2 +240, 50);
+        square(width / 2 -140, height / 2 +240, 50);
+        square(width / 2 -260, height / 2 +240, 50);
 
         // WASD Controls
         fill("#FFFFFF");
         textSize(28);
-        text("W", width / 2 +200, height / 2 +140);
-        text("A S D", width / 2 +200, height / 2 +200);
+        text("W", width / 2 +200, height / 2 +180);
+        text("A S D", width / 2 +200, height / 2 +240);
         noFill();
         strokeWeight(2);
         stroke(255);
         rectMode(CENTER);
-        square(width / 2 +200, height / 2 +140, 50);
-        square(width / 2 +200, height / 2 +200, 50);
-        square(width / 2 +140, height / 2 +200, 50);
-        square(width / 2 +260, height / 2 +200, 50);
+        square(width / 2 +200, height / 2 +180, 50);
+        square(width / 2 +200, height / 2 +240, 50);
+        square(width / 2 +140, height / 2 +240, 50);
+        square(width / 2 +260, height / 2 +240, 50);
         
         noStroke();
         this.returnToStartMenu.draw();
