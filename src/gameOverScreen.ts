e4b72437838efae900839aaf94468b611e191490
ahmad mohammadi <ahmmoh0210@gmail.com>
2025-01-29 08:38:38 +0100

    added restart button

--- src/gameOverScreen.ts
+++ src/gameOverScreen.ts
@@ -1,11 +1,24 @@
 class GameOverScreen extends GameScreen {
+  restartButton: Button;
+
   constructor() {
     super();
+    this.restartButton = new Button(
+      "Restart",
+      createVector(width / 2, height / 2 + 50),
+      "white",
+      createVector(250, 50),
+      "red"
+    )
   }
 
   update(): void {
     // Här kan du lägga till logik om du vill uppdatera något under Game Over
+    if (this.restartButton.isClicked()) {
+      game.changeScreen(new GameBoard(createVector(800, 600)))
+    }
   }
+
   draw(): void {
     push();
 
@@ -14,13 +27,17 @@ class GameOverScreen extends GameScreen {
 
     fill(0, 0, 0, 150); // Transparent svart bakgrund
     // Ritar bakgrunden med rätt dimensioner, så att den täcker texten
-    rect(width / 2 - 0, height / 2 - 0, 300, 200, 10);
+    rect(width / 2 - 0, height / 2 - 0, 350, 200, 10);
 
     fill("white"); // Textfärg vit
     textSize(32);
     textAlign(CENTER, CENTER);
     // Ritar texten på exakt samma plats som bakgrunden
     text("GAME OVER", width / 2, height / 2);
+
+    // Draw restart button
+    this.restartButton.draw();
+
     pop();
   }
 }
