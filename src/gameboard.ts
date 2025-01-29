10a6f27a5813f1243d698592a64009984ff46e18
tomasis7 <tomasis7@gmail.com>
2025-01-29 11:35:40 +0100

    Adjust player starting positions on the gameboard

--- src/gameboard.ts
+++ src/gameboard.ts
@@ -16,13 +16,13 @@ class GameBoard extends GameScreen {
   constructor() {
     super(); // Anropa basklassens konstruktor
     this.players = [
-      new Player(createVector(0, 192), 1, "red", "green", {
+      new Player(createVector(128, 192), 1, "red", "green", {
         UP: UP_ARROW,
         DOWN: DOWN_ARROW,
         RIGHT: RIGHT_ARROW,
         LEFT: LEFT_ARROW,
       }),
-      new Player(createVector(0, 576), 2, "blue", "orange", {
+      new Player(createVector(128, 576), 2, "blue", "orange", {
         UP: 87,
         DOWN: 83,
         RIGHT: 68,
