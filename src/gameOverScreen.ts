fa0ef5bf23e0d1318ce12944a51b769c220b3508
tomasis7 <tomasis7@gmail.com>
2025-01-29 10:27:00 +0100

    fix aligment issue gameover

--- src/gameOverScreen.ts
+++ src/gameOverScreen.ts
@@ -9,13 +9,13 @@ class GameOverScreen extends GameScreen {
       "white",
       createVector(250, 50),
       "red"
-    )
+    );
   }
 
   update(): void {
     // Här kan du lägga till logik om du vill uppdatera något under Game Over
     if (this.restartButton.isClicked()) {
-      game.changeScreen(new GameBoard())
+      game.changeScreen(new GameBoard());
     }
   }
 
@@ -27,7 +27,8 @@ class GameOverScreen extends GameScreen {
 
     fill(0, 0, 0, 150); // Transparent svart bakgrund
     // Ritar bakgrunden med rätt dimensioner, så att den täcker texten
-    rect(width / 2 - 0, height / 2 - 0, 350, 200, 10);
+    rect(width / 2 - 175, height / 2 - 50, 350, 200, 10);
+    // rect(width / 2 - 0, height / 2 - 0, 350, 200, 10);
 
     fill("white"); // Textfärg vit
     textSize(32);
