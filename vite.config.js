52aa656f7b40a472d46a91dec2bc03e16feb8e24
tomasis7 <tomasis7@gmail.com>
2025-02-03 20:11:29 +0100

    Remove unused game-related files and update deployment paths in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 export default defineConfig({
   base: "/Snake2D_p5js_OOP_game/",
   build: {
-    outDir: "build",
+    outDir: "dist",
     rollupOptions: {
       output: {
         entryFileNames: "bundle.js",
