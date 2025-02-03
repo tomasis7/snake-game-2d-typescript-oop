dc222c61ade30f99840cdaf23e58932d24df3b08
tomasis7 <tomasis7@gmail.com>
2025-02-03 19:51:06 +0100

    Replace index-od_mmYbL.js with bundle.js and update HTML reference; configure Vite to output bundle.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   base: "/Snake2D_p5js_OOP_game/",
   build: {
     outDir: "build",
+    rollupOptions: {
+      output: {
+        entryFileNames: "bundle.js",
+      },
+    },
   },
 });
