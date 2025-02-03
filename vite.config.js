156f804bc7de3e4bfe118a554816d69fd422b6d7
tomasis7 <tomasis7@gmail.com>
2025-02-03 21:36:11 +0100

    Enhance project configuration and update HTML structure for Snake 2D game

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ export default defineConfig({
   base: "/Snake2D_p5js_OOP_game/",
   build: {
     outDir: "dist",
+    sourcemap: true,
+    assetsDir: "assets",
+  },
+  server: {
+    open: true,
   },
 });
